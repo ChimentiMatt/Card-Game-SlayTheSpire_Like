34a512cc1a7bfcbf5382e9f0c626a1ad10ac373a
matthew chimenti <matthewosu@gmail.com>
2022-11-22 21:40:41 -0800

    more making of the early card system

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import Hand from './components/Hand';
 import Creature from './components/Creature';
 import './styles.css'
 
-
 function App() {
-  let uuid = 4
+  const [uuid, setUuid] = useState(4)
+  const [health, setHealth] = useState(20)
+  const [energy, setEnergy] = useState(2)
   const [cards, setCards] = useState([
     { id: 1, str: 1, def: 2},
     { id: 2, str: 2, def: 3},
     { id: 3, str: 3, def: 3},
   ])
-  
 
   const [discardPile, setDiscardPile] = useState([])
 
   const [creatureObj, setCreatureObj] = useState({hp: 20})
 
   const draw = () => {
-        // setCards(current => [...current,{ str: 4, def: 3},])
-          setCards(current => [...current, createCard(),])
-      
+    console.log(cards.length)
+    // setCards(current => [...current,{ str: 4, def: 3},])
+    if (cards.length < 6){
+      setCards(current => [...current, createCard(),])
+    }
   }
 
   const playCard = (damage, card) => {
@@ -36,16 +38,17 @@ function App() {
         return element.id !== card.id
     }))
 
-    // add card object to discard pile array
+    // Add a card object to Discard Pile array
     setDiscardPile([...discardPile,{id:card.id, str: card.str}]
     )
 
   }
 
   const createCard = () =>{
-    const str = Math.floor(Math.random() * (6 - 1) + 1); // The maximum is 
-    const def = Math.floor(Math.random() * (6 - 1) + 1); // The maximum is 
-    uuid++
+    const str = Math.floor(Math.random() * (6 - 1) + 1)
+    const def = Math.floor(Math.random() * (6 - 1) + 1)
+
+    setUuid(uuid + 1)
     return { id: uuid, str: str, def: def}
   }
 
@@ -59,16 +62,26 @@ function App() {
     <div className="App">
       <h1>cards</h1>
       <button onClick={testFunc}>TEST</button>
+      <button onClick={draw}>Draw</button>
       
-      <div id='discardPile'>{discardPile.length}</div>
+      <div id='discard-pile'>{discardPile.length}</div>
+
       <Creature creatureObj={creatureObj} setCreatureObj={setCreatureObj}/>
-      <div id="cardContainer">
+
+
+      <div id="card-container">
         {cards.map((card, index) => (
           <Hand key={index} card={card} cards={cards} index={index} playCard={playCard} />
-        ))
-      }
+          ))
+        }
       </div>
-      <button onClick={draw}>Draw</button>
+    
+
+      <div id='health-and-energy-container'>
+        <div id='health'>{health}</div>
+        <div id='energy'>{energy}</div>
+        {/* {uuid} */}
+      </div>
     </div>
   );
 }
--- src/components/Hand.jsx
+++ src/components/Hand.jsx
@@ -6,19 +6,29 @@ const Hand = ({card, cards, index, playCard}) => {
     if (cards.length === 3) rotationArray = [{deg: -3, y: 10}, {deg: 0, y: 0}, {deg: 3, y: 10}]
     else if (cards.length === 4) rotationArray = [{deg: -6, y: 15}, {deg: -2, y: 0}, {deg: 2, y: 0}, {deg: 6, y: 15}]
     else if (cards.length === 5) rotationArray = [{deg: -12, y: 30}, {deg: -6, y: 1}, {deg: 0, y: -10}, {deg: 6, y: 1}, {deg: 12, y: 30}]    
+    else if (cards.length === 6) rotationArray = [{deg: -15, y: 60}, {deg: -10, y: 20}, {deg: -3, y:  1}, {deg: 3, y: 1}, {deg: 10, y: 20}, {deg: 15, y: 60}]    
 
 
     useEffect(() => {
         console.log(`${rotationArray[index].y}`)
     })
 
+    const hoverZoom = (index, onOrOFf) => {
+        console.log(card)
+        if (onOrOFf) gsap.to(`#card${index}`, {zoom: 1.75, y: '-7rem', x: '-3rem'})
+        else gsap.to(`#card${index}`, {zoom: 1, y: '0rem', x: '0rem'})
+    }   
+
 
 
     return (
-    <div className="card card1" onClick={() => playCard(card.str, card)} style={{rotate: `${rotationArray[index].deg}deg`, top: `${rotationArray[index].y}px` }}>
-        {card.str}
-        {/* {index} */}
-        {/* {card.def} */}
+    <div className="card" onClick={() => playCard(card.str, card)} onMouseEnter={() => hoverZoom(index, true)} onMouseLeave={() => hoverZoom(index, false)} style={{rotate: `${rotationArray[index].deg}deg`, top: `${rotationArray[index].y}px` }}>
+        <div id={`card${index}`} className='inner-card'>
+            <p>DMG: {card.str}</p>
+            {/* {index} */}
+            {/* {card.def} */}
+            <p>id: {card.id}</p>
+        </div>
     </div>
   )
 }
